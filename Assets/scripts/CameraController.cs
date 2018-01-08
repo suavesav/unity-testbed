7f087724a0f9b3496ff4beb3aa9bf08afe3cc79a
Savinay Nangalia <savinay.ng@gmail.com>
2018-01-07 16:20:22 -0800

    setup

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+	private Vector3 gap;
+
+	void Start(){
+		gap = transform.position - player.transform.position;
+	}
+
+	void LateUpdate () {
+		transform.position = player.transform.position + gap;
+	}
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed = 5.0F;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		Vector3 appliedForce = new Vector3 (h, 0.0f, v);
+		rb.AddForce(appliedForce * speed);
+	}
+}
